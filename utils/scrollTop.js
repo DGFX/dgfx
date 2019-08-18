33f2a7794d07e3cc66981e71b873bf97eefe64be
Nikodem Zychma <nikodemniq@gmail.com>
2019-08-18 02:00:48 +0200

    init commit

---
+++ utils/scrollTop.js
@@ -0,0 +1,27 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
+    document.getElementById("myBtn").style.display = "block";
+    document.getElementById("navbar").style.backgroundColor = "#5a62ee";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+    document.getElementById("navbar").style.backgroundColor = "#fff";
+  }
+}
+
+function topFunction() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+$(function() {
+    $("#homeBtn").click(function() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    })
+    $("#musicBtn").click(function(){
+        $("html, body").animate({ scrollTop: $(".music-section").position().top }, 250);
+      });
+      $("#contactBtn").click(function(){
+        $("html, body").animate({ scrollTop: $(".contact-section").position().top }, 250);
+      });
+})
